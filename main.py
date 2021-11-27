ff4ca8243c3c8e36a7c422c0ae27708c9c2172a7
mpanahish <masoume.panahi@gmail.com>
2021-11-27 15:16:11 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 import  FactYarTelegramBot
 
 if __name__ == '__main__':
- #   print_hi('PyCharm')
     FactYarTelegramBot.start_telegram_bot()
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
